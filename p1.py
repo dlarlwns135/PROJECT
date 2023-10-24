868c93147fc588bcee4ede0b17717b116bc03d5c
dlarlwns135 <dlarlwns135@naver.com>
2023-10-24 23:37:14 +0900

    점프 구현 반정도 완료
    점프 중 이동하는거 구현중

--- p1.py
+++ p1.py
@@ -7,6 +7,7 @@ import game_world
 # state event check
 # ( state event type, event value )
 
+
 def right_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
 
@@ -27,6 +28,14 @@ def space_down(e):
 
 def time_out(e):
     return e[0] == 'TIME_OUT'
+
+def jump_end(e):
+    return e[0] == 'JUMP_END'
+# def jump_end(e):
+#     global P1
+#     if P1.jump_count >= 40:
+#         return e[0] == 'jump_end'
+#     pass
 
 
 class Idle:
@@ -54,7 +63,10 @@ class Idle:
     @staticmethod
     def draw(p1):
         # p1.image.clip_draw(p1.frame * 34, 0, 30, 64, p1.x, p1.y)
-        p1.sasuke_idle.clip_composite_draw(p1.frame * 34+1, p1.action*64, 30, 64, 0, '', p1.x, p1.y, 100 , 200)
+        if p1.action == 0:
+            p1.sasuke_idle.clip_composite_draw(p1.frame * 32, 0, 32, 64, 0, 'h', p1.x, p1.y, 100, 200)
+        elif p1.action == 1:
+            p1.sasuke_idle.clip_composite_draw(p1.frame * 32, 0, 32, 64, 0, '', p1.x, p1.y, 100 , 200)
 
 
 class Run:
@@ -81,15 +93,22 @@ class Run:
     def draw(p1):
        # p1.sasuke_run.clip_draw(p1.frame * 64, p1.action * 31, 100, 100, p1.x, p1.y)
         if p1.action == 0:
-            p1.sasuke_run.clip_composite_draw(p1.frame * 66, 0, 63, 31, 0, '', p1.x, p1.y, 230, 110)
+            p1.sasuke_run.clip_composite_draw(p1.frame * 64, 0, 64, 32, 0, 'h', p1.x, p1.y, 200, 100)
         elif p1.action == 1:
-            p1.sasuke_run.clip_composite_draw(p1.frame * 66, 0, 63, 31, 0, 'h', p1.x, p1.y, 230, 110)
+            p1.sasuke_run.clip_composite_draw(p1.frame * 64, 0, 64, 32, 0, '', p1.x, p1.y, 200, 100)
+
+
+def key_event(SDL_KEYUP, SDLK_SPACE):
+    pass
+
 
 class Jump:
 
     @staticmethod
     def enter(p1, e):
-        p1.dre = 1
+        p1.jump_count = 0
+        p1.dir = 1
+        p1.frame = 0
         if right_down(e) or left_up(e): # 오른쪽으로 RUN
             p1.dir, p1.face_dir, p1.action = 1, 1, 1
         elif left_down(e) or right_up(e): # 왼쪽으로 RUN
@@ -97,23 +116,40 @@ class Jump:
 
     @staticmethod
     def exit(p1, e):
+        p1.jump_count = 0
+        p1.frame = 0
         pass
 
     @staticmethod
     def do(p1):
         p1.jump_count += 1
-        if p1.jump_count == 10:
+
+        # if right_down(e):
+        #     p1.x += p1.dir * 10  # 오른쪽 방향으로 이동
+        # elif left_down(e):
+        #     p1.x += p1.dir * 10  # 왼쪽 방향으로 이동
+
+        if p1.jump_count % 10 == 0:
             p1.frame = (p1.frame + 1) % 4
+            #p1.jump_count = 0
+
         if p1.frame < 2:
             p1.y += p1.dir * 10
         else:
             p1.y -= p1.dir * 10
+
+        if p1.jump_count >= 40:
+            p1.state_machine.handle_event(('JUMP_END', None))
         delay(0.01)
 
     @staticmethod
     def draw(p1):
-       # p1.sasuke_run.clip_draw(p1.frame * 64, p1.action * 31, 100, 100, p1.x, p1.y)
-       p1.sasuke_run.clip_composite_draw(p1.frame * 66, p1.action * 31, 63, 31, 0, '', p1.x, p1.y, 230, 110)
+        if p1.action == 0:
+            p1.sasuke_jump.clip_composite_draw(p1.frame * 32, 0, 32, 64, 0, 'h', p1.x, p1.y, 100, 200)
+        elif p1.action == 1:
+            p1.sasuke_jump.clip_composite_draw(p1.frame * 32, 0, 32, 64, 0, '', p1.x, p1.y, 100, 200)
+
+
 
 class StateMachine:
     def __init__(self, p1):
@@ -122,7 +158,7 @@ class StateMachine:
         self.transitions = {
             Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, space_down: Jump},
             Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, space_down: Jump},
-
+            Jump: {jump_end: Idle}
         }
 
     def start(self):
@@ -156,8 +192,9 @@ class P1:
         self.action = 3
         self.dir = 0
         self.face_dir = 1
-        self.sasuke_idle = load_image('sasuke_idle_test.png')
+        self.sasuke_idle = load_image('sasuke_idle.png')
         self.sasuke_run = load_image('sasuke_run.png')
+        self.sasuke_jump = load_image('sasuke_jump.png')
         self.state_machine = StateMachine(self)
         self.state_machine.start()
         self.jump_count = 0
