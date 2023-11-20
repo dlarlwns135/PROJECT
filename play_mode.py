8afa9587b566899477a65ff61e5d5702f1100c28
dlarlwns135 <dlarlwns135@naver.com>
2023-11-20 11:18:55 +0900

    mugshot, health bar 틀잡음 굿

--- play_mode.py
+++ play_mode.py
@@ -26,6 +26,12 @@ def init():
     global p1
     global p2
     global map
+    global health_bar, health_hp, naruto_mug, sasuke_mug
+
+    health_bar = load_image('health_bar.png')
+    health_hp = load_image('health_hp.png')
+    naruto_mug = load_image('naruto_mugshot.png')
+    sasuke_mug = load_image('sasuke_mugshot.png')
 
     running = True
     world = []
@@ -74,6 +80,19 @@ def update():
 def draw():
     clear_canvas()
     game_world.render()
-   # delay(0.01)
+    health_bar.clip_composite_draw(0, 0, 402, 22, 0, '', 300, 570, 402, 30)
+    health_bar.clip_composite_draw(0, 0, 402, 22, 0, '', 900, 570, 402, 30)
+    health_hp.clip_composite_draw(0, 0, 8, 9, 0, '', 300-30, 570, 400-60, 28)
+    health_hp.clip_composite_draw(0, 0, 8, 9, 0, '', 900, 570, 400, 28)
+
+    if charactor_choose_mode.p1_choose_result() == 1:
+        naruto_mug.clip_composite_draw(0, 0, 104, 112, 0, '', 1150, 550, 80, 80)
+    elif charactor_choose_mode.p1_choose_result() == 2:
+        sasuke_mug.clip_composite_draw(0, 0, 96, 104, 0, '', 1150, 550, 80, 80)
+
+    if charactor_choose_mode.p2_choose_result() == 1:
+        naruto_mug.clip_composite_draw(0, 0, 104, 112, 0, '', 50, 550, 80, 80)
+    elif charactor_choose_mode.p2_choose_result() == 2:
+        sasuke_mug.clip_composite_draw(0, 0, 96, 104, 0, '', 50, 550, 80, 80)
     update_canvas()
 
